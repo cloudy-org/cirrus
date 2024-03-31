c1f2a871ca52bcbbabd7312de0ba339e1f97452e
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-03-31 12:51:45 +0000

    init

---
+++ tailwind/colours.ts
@@ -0,0 +1,38 @@
+import plugin from "tailwindcss/plugin";
+
+const colours = plugin(
+    ({ addUtilities, addComponents, e, config }) => {},
+    {
+        theme: {
+            extend: {
+                colors: {
+                    exeBlack: "#090b11",
+                    exeGray: "#9ca3af",
+                    goldyPink: "#fb89ab",
+                    goldyDarky: {
+                        DEFAULT: "#0e1114",
+                        200: "#0a0b0d",
+                        300: "#0b0d0f",
+                        500: "#0e1114"
+                    },
+                    goldyGreyy: {
+                        DEFAULT: "#222930",
+                        100: "#222930",
+                        300: "#2A2B2C"
+                    },
+                    goldyCream: "#fbc689",
+                    goldyWhite: "#f1f1f1",
+                    goldyOrangy: {
+                        DEFAULT: "#f5671b",
+                        100: "#f5671b",
+                        300: "#f57d3d",
+                        800: "#f5be3d"
+                    },
+                    goldyGreen: "#d0f54c"
+                }
+            },
+        }
+    },
+);
+
+module.exports = colours;
