9a4e7d53234053a548c85e4663f907411551eaa7
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-05-15 22:31:05 +0100

    fix: user specified theming not working for windows, fixes #1

--- tauri_frontend/theme.ts
+++ tauri_frontend/theme.ts
@@ -1,24 +1,34 @@
-import { platform } from "@tauri-apps/api/os";
-import { invoke } from "@tauri-apps/api/tauri";
 import { getMatches } from "@tauri-apps/api/cli";
+
+type Themes = "light" | "dark" | "auto";
 
 /**
  * Initializes the window theme to the user's specified theme or else it set's it to the operating system theme.
  */
 export function initTheme() {
     getMatches().then((matches) => {
-        let theme = matches.args.theme?.value;
+        const dirty_theme = matches.args.theme?.value;
 
-        if (theme == false || theme == null) {
-            theme = "auto";
+        const theme: Themes = (
+            dirty_theme instanceof Array || typeof dirty_theme == "boolean" || dirty_theme == null
+        )? "auto": dirty_theme as Themes;
+
+        if (theme == "light" || theme == "dark") {
+            localStorage.theme = theme;
+        } else if (theme == "auto") {
+            if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+                localStorage.theme = "dark";
+            } else {
+                localStorage.theme = "light";
+            }
         }
 
-        platform().then((os) => {
-            if (os == "win32") return;
+        console.debug(`Setting '${localStorage.theme}' as the theme...`);
 
-            invoke("plugin:theme|set_theme", {
-                theme: theme,
-            });
-        });
+        if (localStorage.theme == "dark") {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
     });
 }
