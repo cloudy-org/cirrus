7beac95c2b9a3e2106acbf836b4c1fd493ca1e83
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-10-16 23:43:53 +0100

    feat: add theming crate

---
+++ theming/src/lib.rs
@@ -0,0 +1,22 @@
+
+pub struct Theme {
+    pub is_dark: bool,
+    pub hex_code: String
+}
+
+impl Theme {
+    pub fn default(is_dark: bool) -> Self {
+        let hex_code = if is_dark {
+            String::from("#0a0909")
+        } else {
+            String::from("#b3c4c4")
+        };
+
+        Theme { 
+            is_dark, hex_code
+        }
+    }
+}
+
+// Retrieves the system theme... (WIP!)
+// pub fn get_theme() -> Theme {}
