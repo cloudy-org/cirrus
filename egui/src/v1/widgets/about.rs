ae4d3f048e5197a0e59e19577501c250de37ca45
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-12-08 01:00:13 +0000

    feat: add about widget

--- egui/src/v1/widgets/about.rs
+++ egui/src/v1/widgets/about.rs
@@ -1,35 +1,150 @@
-use egui::{ImageSource, Ui};
+use egui::{Color32, Context, ImageSource, Margin, Pos2, Stroke, Ui};
 
 pub struct About<'a> {
     image: ImageSource<'a>,
     info: AboutApplicationInfo,
+    show_license: bool,
 }
 
 pub struct AboutApplicationInfo {
-    name: String,
-    description: String,
-    license: String,
-    version: String,
-    authors: Vec<AboutAuthorInfo>,
-    webpage: String,
-    git_repo: String,
+    pub name: String,
+    pub description: String,
+    pub license: String,
+    pub version: String,
+    pub authors: Vec<AboutAuthorInfo>,
+    pub webpage: String,
+    pub git_repo: String,
+    pub copyright: String,
 }
 
 pub struct AboutAuthorInfo {
-    name: String,
-    email: Option<String>,
-    webpage: Option<String>,
+    pub name: String,
+    pub email: Option<String>,
+    pub webpage: Option<String>,
 }
 
-impl <'a>About<'a> {
+impl<'a> About<'a> {
     pub fn new(image: ImageSource<'a>, info: AboutApplicationInfo) -> Self {
-        Self {
-            image, info,
+        Self { image, info, show_license: false }
+    }
+
+    pub fn update(&mut self, ctx: &Context) {
+        if self.show_license {
+            egui::Window::new(
+                egui::WidgetText::RichText(
+                    egui::RichText::new("ℹ Licence").size(15.0)
+                )
+            )
+                .open(&mut self.show_license)
+                .default_pos(Pos2::new(100.0, 100.0))
+                .show(ctx, |ui| {
+                    egui::ScrollArea::vertical().show(ui, |ui| {
+                        ui.code(self.info.license.clone());
+                    });
+                });
         }
     }
 
-    pub fn show(&self, ui: &mut Ui) {
-        ui.image(self.image.clone());
-        todo!();
+    pub fn show(&mut self, ui: &mut Ui) {
+        let reusable_frame = egui::Frame::canvas(ui.style())
+            .fill(Color32::TRANSPARENT)
+            .stroke(Stroke::NONE);
+
+        ui.vertical_centered(|ui| {
+            ui.add(
+                egui::Image::new(self.image.clone())
+                    .max_width(120.0)
+            );
+
+            ui.heading(
+                egui::RichText::new(self.info.name.clone())
+                    .size(30.0)
+                    .strong()
+                    .monospace()
+            );
+
+            ui.label(
+                egui::RichText::new(format!("v{}", self.info.version))
+                    .monospace()
+            );
+
+            ui.separator();
+
+            ui.horizontal(|ui| {
+                let space = (ui.available_width() - (2.0 * 100.0 + 10.0)) / 2.0;
+
+                ui.add_space(space - 10.0);
+
+                ui.button("Website");
+                ui.button("Source Code");
+                ui.button("Credits");
+            });
+
+            ui.add_space(10.0);
+            ui.heading("Created with ❤ by:");
+
+            reusable_frame.clone()
+                .outer_margin(Margin::symmetric(15.0, 5.0))
+                .show(ui, |ui| {
+                    egui::Grid::new("about_authors_grid")
+                        .num_columns(self.info.authors.len())
+                        .min_row_height(60.0)
+                        .spacing([20.0, 4.0])
+                        .show(ui, |ui| {
+                            // TODO: Make sure we aren't eating crazy amounts of memory with this one. 🔥
+                            let default_image = egui::Image::new(egui::include_image!("../../../../assets/no_author_image.jpg"))
+                                .rounding(100.0);
+        
+                            for author_info in self.info.authors.iter() {
+                                ui.add(default_image.clone()); // TODO: Use actual author's image.
+                                ui.label(
+                                    egui::RichText::new(author_info.name.clone())
+                                        .size(18.0)
+                                );
+                                ui.end_row();
+                            }
+                        }
+                    );
+                }
+            );
+
+            ui.button("❤ and you guys!");
+
+            ui.add_space(20.0);
+
+            if ui.button("License").clicked() {
+                self.show_license = true;
+            }
+            ui.label(&self.info.copyright);
+        });
+
     }
+}
+
+pub fn cargo_authors_to_about_authors(cargo_authors: &String) -> Vec<AboutAuthorInfo> {
+    let mut about_author_infos: Vec<AboutAuthorInfo> = Vec::new();
+
+    for cargo_author in cargo_authors.split(",") {
+        let mut owo = cargo_author.split("<");
+
+        let author_name = owo.next()
+            .expect("Failed to parse cargo authors, expected '<' after name. Please format the authors correctly!")
+            .trim()
+            .to_string();
+
+        let author_email = match owo.next() {
+            Some(email) => Some(email.trim().to_string()),
+            None => None,
+        };
+
+        let about_author_info = AboutAuthorInfo {
+            name: author_name,
+            email: author_email,
+            webpage: None,
+        };
+
+        about_author_infos.push(about_author_info);
+    }
+
+    about_author_infos
 }
