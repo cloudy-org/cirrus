9c524b0d603bd8033de73717c46b773db5a5599b
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2025-03-30 23:09:15 +0100

    fix, feat: should just return the remote version instead of doing any checks with the local version and handle version parse failures

--- egui/src/v1/mod.rs
+++ egui/src/v1/mod.rs
@@ -1,3 +1,2 @@
 pub mod widgets;
 pub mod styling;
-pub mod update;
--- updates/src/v1/mod.rs
+++ updates/src/v1/mod.rs
@@ -1,14 +1,16 @@
 use reqwest::{blocking, header::USER_AGENT, blocking::Response};
-use semver::{Version, VersionReq};
+use semver::Version;
 
-// NOTE: Breaking changes may come to this in the future.
+// NOTE: Breaking changes may come to this in the future as we shift to using other methods.
 
 type ActualError = String;
 
+#[derive(Debug)]
 pub enum Error {
     RequestFailed(ActualError),
     ResponseError(Response),
     FailedDataDeserialization(ActualError),
+    FailedToParseVersion(ActualError),
 }
 
 #[derive(serde::Deserialize)]
@@ -16,7 +18,7 @@ struct GitHubData {
     tag_name: String,
 }
 
-pub fn get_latest_version(username: String, repository: String, local_version: String) -> Result<Option<Version>, Error> {
+pub fn get_latest_version(username: &str, repository: &str) -> Result<Option<Version>, Error> {
     let github_api = format!("https://api.github.com/repos/{}/{}/releases", username, repository);
 
     let client = blocking::Client::new();
@@ -38,19 +40,17 @@ pub fn get_latest_version(username: String, repository: String, local_version: S
 
             match data.first() {
                 Some(newest_release) => {
-                    let mut tag = newest_release.tag_name.clone();
+                    let mut github_tag = newest_release.tag_name.clone();
 
-                    if tag.starts_with("v") {
-                        tag.remove(0);
+                    if github_tag.starts_with("v") {
+                        github_tag.remove(0);
                     }
 
-                    let req_string = format!(">{}", local_version).to_string();
-                    let req = VersionReq::parse(&req_string).unwrap();
-                    let remote = Version::parse(&tag).unwrap();
-
-                    match req.matches(&remote) {
-                        true => Ok(Some(remote)),
-                        false => Ok(None)
+                    match Version::parse(&github_tag) {
+                        Ok(version) => Ok(Some(version)),
+                        Err(error) => Err(
+                            Error::FailedToParseVersion(error.to_string())
+                        ),
                     }
                 }
                 None => Ok(None)
