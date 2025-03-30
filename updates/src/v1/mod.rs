53c713ae3fd9d4ece64c9f824c0b93c1e8e2c787
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2025-03-30 22:24:49 +0100

    feat!: switch away from workspace deps and return error enums instead of strings (untested btw)

--- updates/src/v1/mod.rs
+++ updates/src/v1/mod.rs
@@ -1,29 +1,37 @@
-use reqwest::{blocking, header::USER_AGENT};
+use reqwest::{blocking, header::USER_AGENT, blocking::Response};
 use semver::{Version, VersionReq};
+
+// NOTE: Breaking changes may come to this in the future.
+
+type ActualError = String;
+
+pub enum Error {
+    RequestFailed(ActualError),
+    ResponseError(Response),
+    FailedDataDeserialization(ActualError),
+}
 
 #[derive(serde::Deserialize)]
 struct GitHubData {
     tag_name: String,
 }
 
-pub fn get_latest_version(username: String, repository: String, local_version: String) -> Result<Option<Version>, String> {
+pub fn get_latest_version(username: String, repository: String, local_version: String) -> Result<Option<Version>, Error> {
     let github_api = format!("https://api.github.com/repos/{}/{}/releases", username, repository);
 
     let client = blocking::Client::new();
 
-    match client.get(github_api).header(USER_AGENT, "Reqwest").send() {
+    match client.get(github_api).header(USER_AGENT, "cirrus").send() {
         Ok(resp) => {
             if !resp.status().is_success() {
-                return Err(
-                    format!("GitHub API responded with: {:?}", resp.status())
-                )
+                return Err(Error::ResponseError(resp));
             }
 
             let data: Vec<GitHubData> = match resp.json() {
                 Ok(data) => data,
-                Err(err) => {
+                Err(error) => {
                     return Err(
-                        format!("Failed to deserialize data: {}", err)
+                        Error::FailedDataDeserialization(error.to_string())
                     );
                 }
             };
@@ -31,6 +39,7 @@ pub fn get_latest_version(username: String, repository: String, local_version: S
             match data.first() {
                 Some(newest_release) => {
                     let mut tag = newest_release.tag_name.clone();
+
                     if tag.starts_with("v") {
                         tag.remove(0);
                     }
@@ -39,19 +48,16 @@ pub fn get_latest_version(username: String, repository: String, local_version: S
                     let req = VersionReq::parse(&req_string).unwrap();
                     let remote = Version::parse(&tag).unwrap();
 
-                    if req.matches(&remote) {
-                        return Ok(Some(remote))
-                    } else {
-                        return Ok(None)
+                    match req.matches(&remote) {
+                        true => Ok(Some(remote)),
+                        false => Ok(None)
                     }
                 }
-                None => return Err("No release found".to_string())
-            };
+                None => Ok(None)
+            }
         },
         Err(err) => {
-            return Err(
-                format!("Failed to send request to github api: {}", err.to_string())
-            )
+            Err(Error::RequestFailed(err.to_string()))
         }
-    };
+    }
 }
