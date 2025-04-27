ade8f7c3cdca925ee14a5368f5f09518127b2d0f
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2025-04-27 14:28:44 +0100

    feat, docs: improve egui and toml crate versioning and add example for Scheduler<T> in docstring

--- egui/src/v1/scheduler.rs
+++ egui/src/v1/scheduler.rs
@@ -1,6 +1,37 @@
 use std::time::{Duration, Instant};
 
 /// A neat way to handle scheduling events for later in a egui update loop.
+/// 
+/// ```rust
+/// use std::time::Duration;
+/// use cirrus_egui::v1::scheduler::Scheduler;
+/// 
+/// struct UwUBroadcaster {
+///     uwu_schedule: Scheduler
+/// }
+/// 
+/// impl UwUBroadcaster {
+///     pub fn new() -> Self {
+///         Self {
+///             uwu_schedule: Scheduler::new(
+///                 || {
+///                     // "UwU!" will be printed in the console in 3 seconds time.
+///                     println!("UwU!");
+///                 },
+///                 Duration::from_secs(3)
+///             )
+///         }
+///     }
+/// 
+///     pub fn update(&mut self) {
+///         if self.uwu_schedule.update().is_some() {
+///             // ".update()" will ONLY return "Some()" 
+///             // ONCE, and that is when the callback is called.
+///             println!("Callback has been executed!");
+///         }
+///     }
+/// }
+/// ```
 pub struct Scheduler<T = ()> {
     delay: Duration,
     callback: Box<dyn FnMut() -> T>,
