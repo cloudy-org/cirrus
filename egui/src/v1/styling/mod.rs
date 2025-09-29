d9f37a860ff49437f146f7ebfcf6c4042d0016ed
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2025-09-29 22:21:28 +0100

    refactor!: only visuals is actually necessary for styling #15

--- egui/src/v1/styling/mod.rs
+++ egui/src/v1/styling/mod.rs
@@ -2,29 +2,19 @@ use cirrus_theming::v1::theme::Theme;
 use egui::{Context, Style, TextStyle};
 
 pub mod fonts;
-pub mod windows;
-pub mod background;
 pub(crate) mod visuals;
-pub(crate) mod widgets;
 
 /// Cirrus handled styling for egui... so you don't have to do allat.
-/// 
-/// **WARNING:** `custom_style` does not override anything that is being set in `Styling`!
-/// For example attributes like `.visuals` will not be overridden with `custom_style` if 
-/// `Styling.set_base_visuals()` is invoked. (`.set_base_visuals()` is invoked in `.set_all()`)
 pub struct Styling<'a> {
     theme: &'a Theme,
     pub egui_style: Style,
 }
 
 impl<'a> Styling<'a> {
-    pub fn new(theme: &'a Theme, custom_style: Option<Style>) -> Self {
-        let style = match custom_style {
-            Some(style) => style,
-            None => Style {
-                override_text_style: Some(TextStyle::Monospace),
-                ..Default::default()
-            },
+    pub fn new(theme: &'a Theme) -> Self {
+        let style = Style {
+            override_text_style: Some(TextStyle::Monospace),
+            ..Default::default()
         };
 
         Self {
@@ -36,65 +26,11 @@ impl<'a> Styling<'a> {
     /// Style the entirely of egui to cloudy org styling.
     pub fn set_all(&mut self) -> &Self {
         self
-            .set_base_visuals()
-            .set_background()
-            .set_windows()
-            .set_widgets()
+            .set_visuals()
             .set_fonts(None)
     }
 
     pub fn apply(&self, ctx: &Context) {
         ctx.set_style(self.egui_style.clone());
-    }
-}
-
-impl Styling<'_> {
-    pub fn set_fonts(&mut self, text_style: Option<TextStyle>) -> &mut Self {
-        let text_style = match text_style {
-            Some(text_style) => text_style,
-            None => TextStyle::Monospace,
-        };
-
-        fonts::set_font_style(&mut self.egui_style, text_style, &self.theme.pallet.text);
-
-        self
-    }
-
-    pub fn set_background(&mut self) -> &mut Self {
-        background::set_background_style(
-            &mut self.egui_style,
-            &self.theme.pallet.primary
-        );
-
-        self
-    }
-
-    /// MUST be set FIRST.
-    pub fn set_base_visuals(&mut self) -> &mut Self {
-        visuals::set_base_visuals_style(&mut self.egui_style, self.theme.is_dark);
-
-        self
-    }
-
-    pub fn set_widgets(&mut self) -> &mut Self {
-        widgets::set_widgets_style(
-            &mut self.egui_style,
-            &self.theme.pallet.primary,
-            &self.theme.pallet.surface,
-            &self.theme.pallet.text,
-            &self.theme.pallet.accent
-        );
-
-        self
-    }
-
-    pub fn set_windows(&mut self) -> &mut Self {
-        windows::set_windows_style(
-            &mut self.egui_style,
-            &self.theme.pallet.primary,
-            &self.theme.pallet.surface
-        );
-
-        self
     }
 }
--- egui/src/v1/styling/visuals.rs
+++ egui/src/v1/styling/visuals.rs
@@ -1,12 +1,69 @@
-use egui::{Style, Visuals};
+use egui::{style::{Selection, Widgets}, Color32, CornerRadius, Shadow, Stroke, Visuals};
 
-/// MUST be set FIRST.
-pub fn set_base_visuals_style<'a>(
-    style: &'a mut Style,
-    is_dark: bool
-) {
-    style.visuals = match is_dark {
-        true => Visuals::dark(),
-        false => Visuals::light()
-    };
+use crate::v1::styling::Styling;
+
+impl Styling<'_> {
+    pub fn set_visuals(&mut self) -> &mut Self {
+        let is_dark = self.theme.is_dark;
+        let colour_pallet = &self.theme.pallet;
+
+        let primary_colour = Color32::from_hex(&colour_pallet.primary.as_hex_string()).unwrap();
+        let surface_colour = Color32::from_hex(&colour_pallet.surface.as_hex_string()).unwrap();
+        let text_colour = Color32::from_hex(&colour_pallet.text.as_hex_string()).unwrap();
+        let accent_colour = Color32::from_hex(&colour_pallet.accent.as_hex_string()).unwrap();
+    
+        let secondary_accent_colour = primary_colour.blend(
+            surface_colour.gamma_multiply(0.7)
+        );
+    
+        // Derive stroke colour from subtle text colour.
+        let custom_stroke_colour = text_colour.gamma_multiply(0.4);
+    
+        let default_visuals = match is_dark {
+            true => Visuals::dark(),
+            false => Visuals::light()
+        };
+    
+        let mut widgets = match is_dark {
+            true => Widgets::dark(),
+            false => Widgets::light()
+        };
+    
+        widgets.inactive.bg_fill = secondary_accent_colour;
+        widgets.active.bg_fill = secondary_accent_colour;
+        widgets.hovered.bg_fill = secondary_accent_colour;
+    
+        // this is like the outer frame that appears 
+        // on some widgets when you hover on top of it.
+        widgets.hovered.bg_stroke.color = custom_stroke_colour.gamma_multiply(1.2);
+    
+        // button rounded edge
+        widgets.inactive.corner_radius = CornerRadius::same(7);
+        widgets.active.corner_radius = CornerRadius::same(7);
+        widgets.hovered.corner_radius = CornerRadius::same(7);
+        widgets.noninteractive.corner_radius = CornerRadius::same(7);
+        widgets.open.corner_radius = CornerRadius::same(7);
+    
+        self.egui_style.visuals = Visuals { 
+            dark_mode: is_dark,
+            override_text_color: Some(text_colour),
+            widgets: widgets,
+            // Some widgets version of "visuals.widgets.active" is "visuals.selection" 
+            // like with the custom toggle button in cirrus hence this change is so the 
+            // stroke is more visible when the toggle button widget is active.
+            selection: Selection {
+                bg_fill: accent_colour.gamma_multiply(0.95),
+                stroke: Stroke { width: 2.0, color: custom_stroke_colour },
+            },
+            window_shadow: Shadow::NONE,
+            window_fill: primary_colour.blend(surface_colour.gamma_multiply(0.4)),
+            window_stroke: Stroke::new(1.0, surface_colour),
+            window_highlight_topmost: false,
+            panel_fill: primary_colour,
+            slider_trailing_fill: true,
+            ..default_visuals
+        };
+
+        self
+    }
 }
